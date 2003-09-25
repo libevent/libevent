ec70653b61cdf20f4640a9e3d445a2f77c63e738
Niels Provos <provos@gmail.com>
2003-09-25 17:55:17 +0000

    more tests
    svn:r81

---
+++ test/test.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+function setup {
+	 export EVENT_NOKQUEUE=yes
+	 export EVENT_NOPOLL=yes
+	 export EVENT_NOSELECT=yes
+	 export EVENT_NOEPOLL=yes
+	 export EVENT_NORTSIG=yes
+}
+
+function test {
+	if ! ./test-init 2>/dev/null ;
+	then
+		echo Skipping test
+		return
+	fi	
+
+echo -n " test-eof: "
+if ./test-eof >/dev/null ; 
+then 
+	echo OKAY ; 
+else 
+	echo FAILED ; 
+fi
+echo -n " test-weof: "
+if ./test-weof >/dev/null ; 
+then 
+	echo OKAY ; 
+else 
+	echo FAILED ; 
+fi
+echo -n " test-time: "
+if ./test-time >/dev/null ; 
+then 
+	echo OKAY ; 
+else 
+	echo FAILED ; 
+fi
+echo -n " regress: "
+if ./regress >/dev/null ; 
+then 
+	echo OKAY ; 
+else 
+	echo FAILED ; 
+fi
+}
+
+echo "Running tests:"
+
+# Need to do this by hand?
+setup
+unset EVENT_NOKQUEUE
+echo "KQUEUE"
+test
+
+setup
+unset EVENT_NOPOLL
+echo "POLL"
+test
+
+setup
+unset EVENT_NOSELECT
+echo "SELECT"
+test
+
+setup
+unset EVENT_NORTSIG
+echo "RTSIG"
+test
+
+setup
+unset EVENT_NOEPOLL
+echo "EPOLL"
+test
+
+
+
