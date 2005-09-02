acafd9942f4829205229b80ee32f1c3edfc071a6
Niels Provos <provos@gmail.com>
2005-09-02 05:34:01 +0000

    make it run on older shells; from tor user via Nick Mathewson <nickm@freehaven.net>
    svn:r181

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 setup () {
-	 export EVENT_NOKQUEUE=yes
-	 export EVENT_NODEVPOLL=yes
-	 export EVENT_NOPOLL=yes
-	 export EVENT_NOSELECT=yes
-	 export EVENT_NOEPOLL=yes
-	 export EVENT_NORTSIG=yes
+	 EVENT_NOKQUEUE=yes; export EVENT_NOKQUEUE
+	 EVENT_NODEVPOLL=yes; export EVENT_NODEVPOLL
+	 EVENT_NOPOLL=yes; export EVENT_NOPOLL
+	 EVENT_NOSELECT=yes; export EVENT_NOSELECT
+	 EVENT_NOEPOLL=yes; export EVENT_NOEPOLL
+	 EVENT_NORTSIG=yes; export EVENT_NORTSIG
 }
 
 test () {
-	if ! ./test-init 2>/dev/null ;
+	if ./test-init 2>/dev/null ;
 	then
+	        true
+	else
 		echo Skipping test
 		return
 	fi	
@@ -51,31 +53,37 @@ echo "Running tests:"
 # Need to do this by hand?
 setup
 unset EVENT_NOKQUEUE
+export EVENT_NOKQUEUE
 echo "KQUEUE"
 test
 
 setup
 unset EVENT_NODEVPOLL
+export EVENT_NODEVPOLL
 echo "DEVPOLL"
 test
 
 setup
 unset EVENT_NOPOLL
+export EVENT_NOPOLL
 echo "POLL"
 test
 
 setup
 unset EVENT_NOSELECT
+export EVENT_NOSELECT
 echo "SELECT"
 test
 
 setup
 unset EVENT_NORTSIG
+export EVENT_NORTSIG
 echo "RTSIG"
 test
 
 setup
 unset EVENT_NOEPOLL
+export EVENT_NOEPOLL
 echo "EPOLL"
 test
 
