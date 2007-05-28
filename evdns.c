f0d0559c3e01bc378c3cb960013fc34245128291
Niels Provos <provos@gmail.com>
2007-05-28 21:21:59 +0000

    allow DNS server to get access to the IP address for the requestor; from tor cvs via Nick Mathewson
    svn:r362

--- evdns.c
+++ evdns.c
@@ -1788,6 +1788,17 @@ evdns_server_request_drop(struct evdns_server_request *_req)
 	return 0;
 }
 
+// exported function
+int
+evdns_server_request_get_requesting_addr(struct evdns_server_request *_req, struct sockaddr *sa, int addr_len)
+{
+	struct server_request *req = TO_SERVER_REQUEST(_req);
+	if (addr_len < (int)req->addrlen)
+		return -1;
+	memcpy(sa, &(req->addr), req->addrlen);
+	return req->addrlen;
+}
+
 #undef APPEND16
 #undef APPEND32
 
