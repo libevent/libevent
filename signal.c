108ee5f3a6e1b5fe1de32be47ccfcdc02eb4ca9b
Niels Provos <provos@gmail.com>
2007-12-19 04:52:28 +0000

    from trunk:  r15171@tombo:  nickm | 2007-12-06 12:47:47 -0500  Use GCC attributes (where available) to verify printf type-correctness.  Fix some bugs this turned up.
    svn:r603

--- signal.c
+++ signal.c
@@ -249,7 +249,7 @@ evsignal_handler(int sig)
 
 	if(evsignal_base == NULL) {
 		event_warn(
-			"%s: received signal %s, but have no base configured",
+			"%s: received signal %d, but have no base configured",
 			__func__, sig);
 		return;
 	}
