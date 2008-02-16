8cc1037a75c58fc7e2f43263df6d4e0b7fdefd05
Nick Mathewson <nickm@torproject.org>
2008-02-16 16:56:34 +0000

    r14205@tombo:  nickm | 2008-02-16 11:55:57 -0500  Fix bug 1894184: add a CRLF after each chunk when sending chunked HTTP data.  Original patch from propanbutan.
    svn:r637

--- http.c
+++ http.c
@@ -1728,6 +1728,9 @@ evhttp_send_reply_chunk(struct evhttp_request *req, struct evbuffer *databuf)
 				    (unsigned)EVBUFFER_LENGTH(databuf));
 	}
 	evbuffer_add_buffer(req->evcon->output_buffer, databuf);
+	if (req->chunked) {
+		evbuffer_add(req->evcon->output_buffer, "\r\n", 2);
+	}
 	evhttp_write_buffer(req->evcon, NULL, NULL);
 }
 
