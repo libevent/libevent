807ab182d08971651b3cff66353e3145fb8e4ef2
Nick Mathewson <nickm@torproject.org>
2008-02-16 20:49:47 +0000

    r14211@tombo:  nickm | 2008-02-16 15:28:54 -0500  Add new evutil_strtoll() function so we can apply 64-bit content-length patch from Scott Lamb in a portable way.
    svn:r640

--- evutil.c
+++ evutil.c
@@ -46,6 +46,9 @@
 #ifdef HAVE_FCNTL_H
 #include <fcntl.h>
 #endif
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
 #include <errno.h>
 
 #include "evutil.h"
@@ -166,3 +169,30 @@ evutil_make_socket_nonblocking(evutil_socket_t fd)
 	return 0;
 }
 
+ev_int64_t
+evutil_strtoll(const char *s, char **endptr, int base)
+{
+#ifdef HAVE_STRTOLL
+	return (ev_int64_t)strtoll(s, endptr, base);
+#elif SIZEOF_LONG == 8
+	return (ev_int64_t)strtol(s, endptr, base);
+#elif defined(WIN32) && defined(_MSC_VER) && _MSC_VER < 1300
+	/* XXXX on old versions of MS APIs, we only support base
+	 * 10. */
+	ev_int64_t r;
+	if (base != 10)
+		return 0;
+	r = (ev_int64_t) _atoi64(s);
+	while (isspace(*s))
+		++s;
+	while (isdigit(*s))
+		++s;
+	if (endptr)
+		*endptr = (char*) s;
+	return r;
+#elif defined(WIN32)
+	return (ev_int64_t) _strtoi64(s, endptr, base);
+#else
+#error "I don't know how to parse 64-bit integers."
+#endif
+}
