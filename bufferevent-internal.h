682adc443b03a47c849f028474639c5d8254101c
Niels Provos <provos@gmail.com>
2008-04-30 00:09:16 +0000

    support input/output filters for bufferevents
    svn:r748

---
+++ bufferevent-internal.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2008 Niels Provos <provos@citi.umich.edu>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _BUFFEREVENT_INTERNAL_H_
+#define _BUFFEREVENT_INTERNAL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "config.h"
+#include "evutil.h"
+
+struct bufferevent_filter {
+	/** allows chaining of filters */
+	TAILQ_ENTRY(bufferevent_filter) (next);
+
+	/** used for intermediary state either on the input or output path */
+	struct evbuffer *buffer;
+
+	/** initializes the context provided to process */
+	void (*init_context)(void *);
+
+	/** frees any context related to ctx */
+	void (*free_context)(void *);
+
+	enum bufferevent_filter_result (*process)(
+		struct evbuffer *src, struct evbuffer *dst,
+		enum bufferevent_filter_state flags, void *ctx);
+
+	void *ctx;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _BUFFEREVENT_INTERNAL_H_ */
--- include/event2/bufferevent_struct.h
+++ include/event2/bufferevent_struct.h
@@ -64,6 +64,25 @@ struct event_watermark {
 	size_t high;
 };
 
+struct bufferevent_filter;
+
+/* Fix so that ppl dont have to run with <sys/queue.h> */
+#ifndef TAILQ_HEAD
+#define _EVENT_DEFINED_TQHEAD
+#define TAILQ_HEAD(name, type)						\
+struct name {								\
+	struct type *tqh_first;	/* first element */			\
+	struct type **tqh_last;	/* addr of last next element */		\
+}
+#endif /* !TAILQ_HEAD */
+
+TAILQ_HEAD(bufferevent_filterq, bufferevent_filter);
+
+#ifdef _EVENT_DEFINED_TQHEAD
+#undef TAILQ_HEAD
+#undef _EVENT_DEFINED_TQHEAD
+#endif /* _EVENT_DEFINED_TQHEAD */
+
 struct bufferevent {
 	struct event_base *ev_base;
 
@@ -85,8 +104,11 @@ struct bufferevent {
 	int timeout_write;	/* in seconds */
 
 	short enabled;	/* events that are currently enabled */
-};
 
+	/** the list of input and output filters */
+	struct bufferevent_filterq input_filters;
+	struct bufferevent_filterq output_filters;
+};
 
 #ifdef __cplusplus
 }
