85ed7133439658bf7b44a99100fcf82eed1d232e
Niels Provos <provos@gmail.com>
2008-05-08 07:00:16 +0000

    deprecate timeout_* event functions by moving them to event_compat.h
    svn:r791

--- include/event2/event_compat.h
+++ include/event2/event_compat.h
@@ -174,7 +174,33 @@ const char *event_get_method(void);
 int	event_priority_init(int);
 
 
+/**
+ * Add a timeout event.
+ *
+ * @param ev the event struct to be disabled
+ * @param tv the timeout value, in seconds
+ */
+#define timeout_add(ev, tv)		event_add(ev, tv)
 
+
+/**
+ * Define a timeout event.
+ *
+ * @param ev the event struct to be defined
+ * @param cb the callback to be invoked when the timeout expires
+ * @param arg the argument to be passed to the callback
+ */
+#define timeout_set(ev, cb, arg)	event_set(ev, -1, 0, cb, arg)
+
+/**
+ * Disable a timeout event.
+ *
+ * @param ev the timeout event to be disabled
+ */
+#define timeout_del(ev)			event_del(ev)
+
+#define timeout_pending(ev, tv)		event_pending(ev, EV_TIMEOUT, tv)
+#define timeout_initialized(ev)		_event_initialized((ev), 0)
 
 #ifdef __cplusplus
 }
