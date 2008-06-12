d5c3cdca201cac20e4df0983d9a1be11f37698ce
Niels Provos <provos@gmail.com>
2008-06-12 14:45:29 +0000

    from trunk: allow min_heap_erase to be called on removed members; from liusifan
    svn:r850

--- min_heap.h
+++ min_heap.h
@@ -75,8 +75,8 @@ struct event* min_heap_pop(min_heap_t* s)
     if(s->n)
     {
         struct event* e = *s->p;
-        e->min_heap_idx = -1;
         min_heap_shift_down_(s, 0u, s->p[--s->n]);
+        e->min_heap_idx = -1;
         return e;
     }
     return 0;
