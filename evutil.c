ed8dd5beb48f603b6df6f083ca58aaab5c0accc1
Niels Provos <provos@gmail.com>
2008-06-24 16:29:02 +0000

    fix evutil_snprintf on windows
    svn:r860

--- evutil.c
+++ evutil.c
@@ -224,7 +224,7 @@ evutil_snprintf(char *buf, size_t buflen, const char *format, ...)
 	int r;
 	va_list ap;
 	va_start(ap, format);
-	r = vsnprintf(buf, buflen, format, ap);
+	r = evutil_vsnprintf(buf, buflen, format, ap);
 	va_end(ap);
 	return r;
 }
