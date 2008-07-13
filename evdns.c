1b40a378d16b6e61219ca75c3befd64663b49e7c
Niels Provos <provos@gmail.com>
2008-07-13 20:20:10 +0000

    from trunk: fix cname replies
    svn:r908

--- evdns.c
+++ evdns.c
@@ -1581,7 +1581,7 @@ int
 evdns_server_request_add_cname_reply(struct evdns_server_request *req, const char *name, const char *cname, int ttl)
 {
 	return evdns_server_request_add_reply(
-		  req, EVDNS_ANSWER_SECTION, name, TYPE_A, CLASS_INET,
+		  req, EVDNS_ANSWER_SECTION, name, TYPE_CNAME, CLASS_INET,
 		  ttl, -1, 1, cname);
 }
 
