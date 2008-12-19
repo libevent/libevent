f383ee29d9ab1b6999cd85a0da8e2a62cf8c34b3
Niels Provos <provos@gmail.com>
2008-12-19 21:53:23 +0000

    revert: Fix a memory leak with http connections owned by the server.
    svn:r966

--- http.c
+++ http.c
@@ -1092,8 +1092,6 @@ evhttp_detect_close_cb(int fd, short what, void *arg)
 {
 	struct evhttp_connection *evcon = arg;
 	evhttp_connection_reset(evcon);
-	if (evcon->http_server && evcon->state == EVCON_READING_FIRSTLINE)
-		evhttp_connection_free(evcon);
 }
 
 static void
