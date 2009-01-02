ae8893f63fa7225241a7ba76173a2b0e681264fe
Nick Mathewson <nickm@torproject.org>
2009-01-02 04:56:56 +0000

    Include http_compat.h in http.c so we do not get warnings about functions with no prototypes.
    svn:r981

--- http.c
+++ http.c
@@ -89,6 +89,7 @@
 #include "event2/buffer.h"
 #include "event2/bufferevent.h"
 #include "event2/http_struct.h"
+#include "event2/http_compat.h"
 #include "event2/util.h"
 #include "log.h"
 #include "http-internal.h"
