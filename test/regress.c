dc1526e0da9c13eba2138c8a551d2535ede385d8
Nick Mathewson <nickm@torproject.org>
2009-01-22 17:48:16 +0000

    Fix warnings on compile: make static functions static.
    svn:r1037

--- test/regress.c
+++ test/regress.c
@@ -864,7 +864,7 @@ out:
 	return;
 }
 
-void
+static void
 signal_cb_swp(int sig, short event, void *arg)
 {
 	called++;
@@ -873,7 +873,7 @@ signal_cb_swp(int sig, short event, void *arg)
 	else
 		event_loopexit(NULL);
 }
-void
+static void
 timeout_cb_swp(int fd, short event, void *arg)
 {
 	if (called == -1) {
