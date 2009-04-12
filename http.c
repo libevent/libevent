1a51887386e4005fdd8faee02da76f04ffd6b7b8
Niels Provos <provos@gmail.com>
2009-04-12 23:00:52 +0000

    from trunk: improve evhttp_parse_query documentation
    svn:r1161

--- http.c
+++ http.c
@@ -2115,7 +2115,6 @@ evhttp_decode_uri(const char *uri)
 /* 
  * Helper function to parse out arguments in a query.
  * The arguments are separated by key and value.
- * URI should already be decoded.
  */
 
 void
