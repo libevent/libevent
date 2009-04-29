3412e5b174d06bfc342006c5a4712ae8dd9b6c35
Nick Mathewson <nickm@torproject.org>
2009-04-29 20:48:43 +0000

    Note a place we might do better about lock releasing.
    svn:r1252

--- buffer.c
+++ buffer.c
@@ -370,6 +370,8 @@ evbuffer_deferred_callback(struct deferred_cb *cb, void *arg)
 {
 	struct evbuffer *buffer = arg;
 
+	/* XXXX It would be better to run these callbacks without holding the
+	 * lock */
 	EVBUFFER_LOCK(buffer, EVTHREAD_WRITE);
 	evbuffer_run_callbacks(buffer);
 	_evbuffer_decref_and_unlock(buffer);
