5a3eddf03f9a2c2d9bf52326e504be1555e2882f
Nick Mathewson <nickm@torproject.org>
2009-05-02 16:24:05 +0000

    Use fewer _compat.h headers in our own code.
    svn:r1268

--- bufferevent.c
+++ bufferevent.c
@@ -51,7 +51,6 @@
 #include "event2/util.h"
 #include "event2/bufferevent.h"
 #include "event2/buffer.h"
-#include "event2/buffer_compat.h"
 #include "event2/bufferevent_struct.h"
 #include "event2/bufferevent_compat.h"
 #include "event2/event.h"
