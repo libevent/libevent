18fe400805699b6d60bfc26896294a34c6990756
Nick Mathewson <nickm@torproject.org>
2009-09-23 23:51:26 +0000

    Forward-port: fix android compilation
    svn:r1435

--- select.c
+++ select.c
@@ -57,6 +57,13 @@
 #define        howmany(x, y)   (((x)+((y)-1))/(y))
 #endif
 
+#ifndef _EVENT_HAVE_FD_MASK
+/* This type is mandatory, but Android doesn't define it. */
+#undef NFDBITS
+#define NFDBITS (sizeof(long)*8)
+typedef unsigned long fd_mask;
+#endif
+
 struct selectop {
 	int event_fds;		/* Highest fd in fd set */
 	int event_fdsz;
