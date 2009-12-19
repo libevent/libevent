ac0d213c4aad94f73d4fc0c441a1f3d28d691853
Nick Mathewson <nickm@torproject.org>
2009-12-18 23:50:42 -0500

    Correct all versions in 1.4 branch

--- WIN32-Code/event-config.h
+++ WIN32-Code/event-config.h
@@ -207,7 +207,9 @@
 #define _EVENT_TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define _EVENT_VERSION "1.3.99-trunk"
+#define _EVENT_VERSION "1.4.13-stable-dev"
+
+#define _EVENT_NUMERIC_VERSION 0x01040d01
 
 /* Define to appropriate substitue if compiler doesnt have __func__ */
 /* #undef _EVENT___func__ */
