0b151a9fa1365ad9f6f2e58fcfcfb36840707403
Nick Mathewson <nickm@torproject.org>
2009-12-29 18:11:52 -0500

    Whitespace fixes in test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 if test "x$TEST_OUTPUT_FILE" = "x"
 then
-   TEST_OUTPUT_FILE=/dev/null
+	TEST_OUTPUT_FILE=/dev/null
 fi
 
 touch "$TEST_OUTPUT_FILE" || exit 1
@@ -12,63 +12,63 @@ TEST_DIR=.
 T=`echo "$0" | sed -e 's/test.sh$//'`
 if test -x "$T/test-init"
 then
- TEST_DIR="$T"
+	TEST_DIR="$T"
 fi
 
 setup () {
-	 EVENT_NOKQUEUE=yes; export EVENT_NOKQUEUE
-	 EVENT_NODEVPOLL=yes; export EVENT_NODEVPOLL
-	 EVENT_NOPOLL=yes; export EVENT_NOPOLL
-	 EVENT_NOSELECT=yes; export EVENT_NOSELECT
-	 EVENT_NOEPOLL=yes; export EVENT_NOEPOLL
-	 EVENT_NOEVPORT=yes; export EVENT_NOEVPORT
+	EVENT_NOKQUEUE=yes; export EVENT_NOKQUEUE
+	EVENT_NODEVPOLL=yes; export EVENT_NODEVPOLL
+	EVENT_NOPOLL=yes; export EVENT_NOPOLL
+	EVENT_NOSELECT=yes; export EVENT_NOSELECT
+	EVENT_NOEPOLL=yes; export EVENT_NOEPOLL
+	EVENT_NOEVPORT=yes; export EVENT_NOEVPORT
 }
 
 announce () {
-    echo $@
-    echo $@ >>"$TEST_OUTPUT_FILE"
+	echo $@
+	echo $@ >>"$TEST_OUTPUT_FILE"
 }
 
 run_tests () {
 	if $TEST_DIR/test-init 2>>"$TEST_OUTPUT_FILE" ;
 	then
-	        true
+		true
 	else
 		announce Skipping test
 		return
-	fi	
+	fi
 
-announce -n " test-eof: "
-if $TEST_DIR/test-eof >>"$TEST_OUTPUT_FILE" ; 
-then 
-	announce OKAY ; 
-else 
-	announce FAILED ; 
-fi
-announce -n " test-weof: "
-if $TEST_DIR/test-weof >>"$TEST_OUTPUT_FILE" ; 
-then 
-	announce OKAY ; 
-else 
-	announce FAILED ; 
-fi
-announce -n " test-time: "
-if $TEST_DIR/test-time >>"$TEST_OUTPUT_FILE" ; 
-then 
-	announce OKAY ; 
-else 
-	announce FAILED ; 
-fi
-announce -n " regress: "
-if $TEST_DIR/regress >>"$TEST_OUTPUT_FILE" ; 
-then 
-	announce OKAY ; 
-else 
-	announce FAILED ; 
-fi
+	announce -n " test-eof: "
+	if $TEST_DIR/test-eof >>"$TEST_OUTPUT_FILE" ;
+	then
+		announce OKAY ;
+	else
+		announce FAILED ;
+	fi
+	announce -n " test-weof: "
+	if $TEST_DIR/test-weof >>"$TEST_OUTPUT_FILE" ;
+	then
+		announce OKAY ;
+	else
+		announce FAILED ;
+	fi
+	announce -n " test-time: "
+	if $TEST_DIR/test-time >>"$TEST_OUTPUT_FILE" ;
+	then
+		announce OKAY ;
+	else
+		announce FAILED ;
+	fi
+	announce -n " regress: "
+	if $TEST_DIR/regress >>"$TEST_OUTPUT_FILE" ;
+	then
+		announce OKAY ;
+	else
+		announce FAILED ;
+	fi
 }
 
-announce "Running run_testss:"
+announce "Running tests:"
 
 # Need to do this by hand?
 setup
@@ -106,6 +106,3 @@ unset EVENT_NOEVPORT
 export EVENT_NOEVPORT
 announce "EVPORT"
 run_tests
-
-
-
