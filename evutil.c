4df7dbcbdfc9bf169f70b3e74e0a3ec824065bf3
Jardel Weyrich <jweyrich@gmail.com>
2009-12-29 16:19:24 -0200

    Adjusted fcntl() retval comparison on evutil_make_socket_nonblocking().
    Apparently, a successful return value on F_SETFL is "anything but
    -1".

--- evutil.c
+++ evutil.c
@@ -187,11 +187,11 @@ evutil_make_socket_nonblocking(evutil_socket_t fd)
 	{
 		long flags;
 		if ((flags = fcntl(fd, F_GETFL, NULL)) < 0) {
-			event_warn("fcntl(F_GETFL)");
+			event_warn("fcntl(%d, F_GETFL)", fd);
 			return -1;
 		}
-		if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
-			event_warn("fcntl(O_NONBLOCK)");
+		if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+			event_warn("fcntl(%d, F_SETFL)", fd);
 			return -1;
 		}
 	}
