d89fdba424c654445d0f927670691d9b55f20846
Nick Mathewson <nickm@torproject.org>
2010-07-05 12:28:22 -0400

    Make sample/hello_world	work on	windows
    We forgot to include the WSAStartup call in main().
    
    Patch from an anonymous user on Sourceforge.
    
    Fixes bug 3025354.

--- sample/hello-world.c
+++ sample/hello-world.c
@@ -38,9 +38,12 @@ main(int argc, char **argv)
 	struct event_base *base;
 	struct evconnlistener *listener;
 	struct event *signal_event;
-	// struct sockaddr_storage ss;
 
 	struct sockaddr_in sin;
+#ifdef WIN32
+	WSADATA wsa_data;
+	WSAStartup(0x0201, &wsa_data);
+#endif
 
 	base = event_base_new();
 	if (!base) {
