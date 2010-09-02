ca9048f12cf441625ac3e90edb219d7f4c60b430
Nick Mathewson <nickm@torproject.org>
2010-09-02 11:36:44 -0400

    Move evkeyvalq into a separate header for evhttp_parse_query users
    The evhttp_parse_query API is a bit misdesigned; all the other
    evkeyvalq stuff is abstract and lets you get away with having a header
    stub, but evhttp_parse_query seems to require that you instantiate an
    empty evkeyvalq of your own.

--- include/event2/keyvalq_struct.h
+++ include/event2/keyvalq_struct.h
@@ -24,45 +24,15 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef _EVENT2_EVENT_STRUCT_H_
-#define _EVENT2_EVENT_STRUCT_H_
-
-/** @file event_struct.h
-
-  Structures used by event.h.  Using these structures directly may harm
-  forward compatibility: be careful!
-
- */
+#ifndef _EVENT2_EVENT_KEYVALQ_STRUCT_H_
+#define _EVENT2_EVENT_KEYVALQ_STRUCT_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <event2/event-config.h>
-#ifdef _EVENT_HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-#ifdef _EVENT_HAVE_SYS_TIME_H
-#include <sys/time.h>
-#endif
-
-/* For int types. */
-#include <event2/util.h>
-
-/* For evkeyvalq */
-#include <event2/keyvalq_struct.h>
-
-#define EVLIST_TIMEOUT	0x01
-#define EVLIST_INSERTED	0x02
-#define EVLIST_SIGNAL	0x04
-#define EVLIST_ACTIVE	0x08
-#define EVLIST_INTERNAL	0x10
-#define EVLIST_INIT	0x80
-
-/* EVLIST_X_ Private space: 0x1000-0xf000 */
-#define EVLIST_ALL	(0xf000 | 0x9f)
-
 /* Fix so that people don't have to run with <sys/queue.h> */
+/* XXXX This code is duplicated with event_struct.h */
 #ifndef TAILQ_ENTRY
 #define _EVENT_DEFINED_TQENTRY
 #define TAILQ_ENTRY(type)						\
@@ -81,49 +51,20 @@ struct name {					\
 }
 #endif
 
-struct event_base;
-struct event {
-	TAILQ_ENTRY(event) ev_active_next;
-	TAILQ_ENTRY(event) ev_next;
-	/* for managing timeouts */
-	union {
-		TAILQ_ENTRY(event) ev_next_with_common_timeout;
-		int min_heap_idx;
-	} ev_timeout_pos;
-	evutil_socket_t ev_fd;
+/*
+ * Key-Value pairs.  Can be used for HTTP headers but also for
+ * query argument parsing.
+ */
+struct evkeyval {
+	TAILQ_ENTRY(evkeyval) next;
 
-	struct event_base *ev_base;
-
-	union {
-		/* used for io events */
-		struct {
-			TAILQ_ENTRY(event) ev_io_next;
-			struct timeval ev_timeout;
-		} ev_io;
-
-		/* used by signal events */
-		struct {
-			TAILQ_ENTRY(event) ev_signal_next;
-			short ev_ncalls;
-			/* Allows deletes in callback */
-			short *ev_pncalls;
-		} ev_signal;
-	} _ev;
-
-	short ev_events;
-	short ev_res;		/* result passed to event callback */
-	short ev_flags;
-	ev_uint8_t ev_pri;	/* smaller numbers are higher priority */
-	ev_uint8_t ev_closure;
-	struct timeval ev_timeout;
-
-	/* allows us to adopt for different types of events */
-	void (*ev_callback)(evutil_socket_t, short, void *arg);
-	void *ev_arg;
+	char *key;
+	char *value;
 };
 
-TAILQ_HEAD (event_list, event);
+TAILQ_HEAD (evkeyvalq, evkeyval);
 
+/* XXXX This code is duplicated with event_struct.h */
 #ifdef _EVENT_DEFINED_TQENTRY
 #undef TAILQ_ENTRY
 #endif
@@ -136,4 +77,4 @@ TAILQ_HEAD (event_list, event);
 }
 #endif
 
-#endif /* _EVENT2_EVENT_STRUCT_H_ */
+#endif
