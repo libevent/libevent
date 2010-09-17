90651b327a9819e890e8ababe1e46f3fa989850c
Nick Mathewson <nickm@torproject.org>
2010-09-17 00:24:50 -0400

    Put internal events at highest priority
    (If we allow user events to starve internal events, then internal events
    never actually happen, signals don't get acked, etc)

--- signal.c
+++ signal.c
@@ -136,6 +136,7 @@ evsig_init(struct event_base *base)
 		EV_READ | EV_PERSIST, evsig_cb, &base->sig.ev_signal);
 
 	base->sig.ev_signal.ev_flags |= EVLIST_INTERNAL;
+	event_priority_set(&base->sig.ev_signal, 0);
 
 	base->evsigsel = &evsigops;
 	base->evsigbase = &base->sig;
