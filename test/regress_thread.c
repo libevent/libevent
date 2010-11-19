3a67d0bf42c69423fca36d001023d563b0326399
Nick Mathewson <nickm@torproject.org>
2010-11-19 17:09:30 -0500

    Resolve an evport bug in the thread/forking test

--- test/regress_thread.c
+++ test/regress_thread.c
@@ -214,10 +214,15 @@ thread_basic(void *arg)
 		event_add(notification_event, NULL);
 
 		if ((pid = fork()) == 0) {
+			event_del(notification_event);
 			if (event_reinit(base) < 0) {
 				TT_FAIL(("reinit"));
 				exit(1);
 			}
+			event_assign(notification_event, base,
+			    base->th_notify_fd[0], EV_READ|EV_PERSIST,
+			    notify_fd_cb, NULL);
+			event_add(notification_event, NULL);
 	 		goto child;
 		}
 
