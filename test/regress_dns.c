48c44a6ddaf995ddbb8cc93d07beedf113f5cbac
Sebastian Hahn <sebastian@torproject.org>
2011-04-11 17:48:23 +0200

    Refactor test_getaddrinfo_async_cancel_stress()
    Turns out it didn't need an event base set up.
    
    Spotted by clang's static anaylzer

--- test/regress_dns.c
+++ test/regress_dns.c
@@ -1626,10 +1626,9 @@ gaic_launch(struct event_base *base, struct evdns_base *dns_base)
 }
 
 static void
-test_getaddrinfo_async_cancel_stress(void *arg)
+test_getaddrinfo_async_cancel_stress(void *ptr)
 {
-	struct basic_test_data *data = arg;
-	struct event_base *base = data->base;
+	struct event_base *base;
 	struct evdns_base *dns_base = NULL;
 	struct evdns_server_port *server = NULL;
 	evutil_socket_t fd = -1;
@@ -1701,7 +1700,7 @@ struct testcase_t dns_testcases[] = {
 	{ "getaddrinfo_async", test_getaddrinfo_async,
 	  TT_FORK|TT_NEED_BASE, &basic_setup, (char*)"" },
 	{ "getaddrinfo_cancel_stress", test_getaddrinfo_async_cancel_stress,
-	  TT_FORK|TT_NEED_BASE, &basic_setup, (char*)"" },
+	  TT_FORK, NULL, NULL },
 
 	END_OF_TESTCASES
 };
