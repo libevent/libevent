libevent/libevent

commit 2aa036fa0403977fa51812e711a1d5040c9fb5c9

Avoid spinning on OpenSSL reads
Previously, if some sender were generating data to read on an
OpenSSL connection as fast as we could process it, we could easily
wind up looping on an openssl do_read operation without ever
considering other sockets.

The difference between this and the original method in
consider_reading() is that it only loops for a single completed
*frame* instead of looping until fd is drained or an error condition
was triggered.

{Patch split out by nickm}

bufferevent_openssl.c
@@ -766,10 +766,13 @@ consider_reading(struct bufferevent_openssl *bev_ssl)
 	if (bev_ssl->write_blocked_on_read)
 		return;
 
-	while ((n_to_read = bytes_to_read(bev_ssl)) != 0) {
-		r = do_read(bev_ssl, n_to_read);
-		if (r <= 0)
+	n_to_read = bytes_to_read(bev_ssl);
+
+	while (n_to_read) {
+		if (do_read(bev_ssl, n_to_read) <= 0)
 			break;
+
+		n_to_read = SSL_pending(bev_ssl->ssl);
 	}
 
 	if (!bev_ssl->underlying) {

commit c58d9d8d437d122a8b67d42a6126d81699ec6914

Make sure the input buffers in a filtered openssl connection are drained properly in consider_read. SSL_pending() doesn't always return the correct number of bytes to read from the underlying buffer event in the case of filtered openssl connections. Fixes regression introduced by 2aa036fa .

bufferevent_openssl.c
@@ -783,9 +783,14 @@ consider_reading(struct bufferevent_openssl *bev_ssl)
 		 * regrettable).  The damage to the rate-limit has
 		 * already been done, since OpenSSL went and read a
 		 * whole SSL record anyway. */
-		n_to_read = SSL_pending(bev_ssl->ssl);
+        if (bev_ssl->underlying) {
+            n_to_read = evbuffer_get_length(bev_ssl->underlying->input);
+        }
+        else { 
+            n_to_read = SSL_pending(bev_ssl->ssl);
+        }
 	}
-
+	
 	if (!bev_ssl->underlying) {
 		/* Should be redundant, but let's avoid busy-looping */
 		if (bev_ssl->bev.read_suspended ||