ac356502d313ff7712a43e2e8361d7302d8f8c0c
Patrick Pelletier <code@funwithsoftware.org>
2012-11-07 19:48:22 -0800

    remove stray 'x' so print_err will compile when uncommented

--- bufferevent_openssl.c
+++ bufferevent_openssl.c
@@ -87,7 +87,7 @@ print_err(int val)
 	int err;
 	printf("Error was %d\n", val);
 
-	while ((err = ERR_get_error()))x {
+	while ((err = ERR_get_error())) {
 		const char *msg = (const char*)ERR_reason_error_string(err);
 		const char *lib = (const char*)ERR_lib_error_string(err);
 		const char *func = (const char*)ERR_func_error_string(err);
