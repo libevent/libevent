dd4be76f8316a3eb899b4cfe9479bc7619db44f0
Nick Mathewson <nickm@torproject.org>
2013-04-30 12:02:49 -0400

    Incremement version to 2.1.3-alpha

--- WIN32-Code/event2/event-config.h
+++ WIN32-Code/event2/event-config.h
@@ -277,7 +277,7 @@
 /* #undef EVENT__HAVE_WORKING_KQUEUE */
 
 /* Numeric representation of the version */
-#define EVENT__NUMERIC_VERSION 0x02010201
+#define EVENT__NUMERIC_VERSION 0x02010300
 
 /* Name of package */
 #define EVENT__PACKAGE "libevent"
@@ -334,7 +334,7 @@
 #define EVENT__TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define EVENT__VERSION "2.1.2-alpha-dev"
+#define EVENT__VERSION "2.1.3-alpha"
 
 /* Define to appropriate substitue if compiler doesnt have __func__ */
 #define EVENT____func__ __FUNCTION__
