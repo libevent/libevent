f7805939d161412e0c25cbddb71f9f329beb752b
Joakim Soderberg <joakim.soderberg@gmail.com>
2013-12-19 10:46:04 +0000

    Only look for ZLib when it is used (if tests are included).
    Also make the project language C.

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -68,14 +68,14 @@ IF(NOT GCOV_PATH)
 	MESSAGE(FATAL_ERROR "gcov not found! Aborting...")
 ENDIF() # NOT GCOV_PATH
 
-IF(NOT CMAKE_COMPILER_IS_GNUCXX)
+IF(NOT CMAKE_COMPILER_IS_GNUCC AND NOT CMAKE_COMPILER_IS_GNUCXX)
 	# Clang version 3.0.0 and greater now supports gcov as well.
 	MESSAGE(WARNING "Compiler is not GNU gcc! Clang Version 3.0.0 and greater supports gcov as well, but older versions don't.")
 	
 	IF(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 		MESSAGE(FATAL_ERROR "Compiler is not GNU gcc! Aborting...")
 	ENDIF()
-ENDIF() # NOT CMAKE_COMPILER_IS_GNUCXX
+ENDIF() # NOT CMAKE_COMPILER_IS_GNUCC
 
 IF ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
   MESSAGE( WARNING "Code coverage results with an optimized (non-Debug) build may be misleading" )
