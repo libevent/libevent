f3100ac34f290d66b54542616c93c884366b8c44
william <william@25tandclement.com>
2014-01-08 17:42:40 -0800

    add loadable implementations for heap and wheel

---
+++ bench-wheel.c
@@ -0,0 +1,46 @@
+#include "timeout.h"
+#include "timeout.c"
+#include "bench.h"
+
+
+static struct timeouts timeouts;
+
+static void init(struct timeout *timeout, size_t count) {
+	size_t i;
+
+	timeouts_init(&timeouts, TIMEOUT_mHZ);
+
+	for (i = 0; i < count; i++) {
+		timeout_init(&timeout[i], 0);
+	}
+} /* init() */
+
+
+static void add(struct timeout *to, timeout_t expires) {
+	timeouts_add(&timeouts, to, expires);
+} /* add() */
+
+
+static void del(struct timeout *to) {
+    timeouts_del(&timeouts, to);
+} /* del() */
+
+
+static struct timeout *get(void) {
+	return timeouts_get(&timeouts);
+} /* get() */
+
+
+static void update(timeout_t ts) {
+	timeouts_update(&timeouts, ts);
+} /* update() */
+
+
+const struct vops VOPS = {
+    .init   = &init,
+    .add    = &add,
+    .del    = &del,
+    .get    = &get,
+    .update = &update,
+};
+
