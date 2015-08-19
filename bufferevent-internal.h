libevent/libevent

commit c42bc6b279fd7cd4bc9b04c86a12e2ec5512cce1

be: fix sockaddr_in6 type definition for win32
bufferevent-internal.h:217:23: error: field 'in6' has incomplete type
   struct sockaddr_in6 in6;
                       ^
CI: https://ci.appveyor.com/project/nmathewson/libevent/build/2.1.5.108

bufferevent-internal.h
@@ -39,6 +39,7 @@ extern "C" {
 #include "event2/thread.h"
 #include "ratelim-internal.h"
 #include "event2/bufferevent_struct.h"
+#include "ipv6-internal.h"
 
 /* These flags are reasons that we might be declining to actually enable
    reading or writing on a bufferevent.

commit c21229192f9dd1dd40b6c8d26adf7f9502625ede

be: include all variations of headers for sockaddr_in6 struct
Fixes: c42bc6b279fd7cd4bc9b04c86a12e2ec5512cce1 ("be: fix sockaddr_in6 type
definition for win32")
Fixes: e04555155816306f23829a15b747664a57330c28 ("Merge branch
'reuse-conn_address-on-retry-v11'")
CI: https://ci.appveyor.com/project/nmathewson/libevent/build/2.1.5.109
Tested: manually

bufferevent-internal.h
@@ -39,7 +39,14 @@ extern "C" {
 #include "event2/thread.h"
 #include "ratelim-internal.h"
 #include "event2/bufferevent_struct.h"
+
 #include "ipv6-internal.h"
+#ifdef _WIN32
+#include <ws2tcpip.h>
+#endif
+#ifdef EVENT__HAVE_NETINET_IN6_H
+#include <netinet/in6.h>
+#endif
 
 /* These flags are reasons that we might be declining to actually enable
    reading or writing on a bufferevent.