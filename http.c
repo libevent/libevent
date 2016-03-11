libevent/libevent

commit 2185e639210f072f37e9d19aff7dba382db84529

http: assert's that evbuffer_drain() success on connection reset
Since otherwise we can have nasty bugs with part of previous *request* in
current *request* and hence some parsing errors.

And now we have failures:
  http/non_lingering_close: [forking] [err] ../http.c:1326: Assertion !evbuffer_drain(tmp, -1) failed in ../http.c

http.c
@@ -1323,9 +1323,9 @@ evhttp_connection_reset_(struct evhttp_connection *evcon)
 
 	/* we need to clean up any buffered data */
 	tmp = bufferevent_get_output(evcon->bufev);
-	evbuffer_drain(tmp, evbuffer_get_length(tmp));
+	EVUTIL_ASSERT(!evbuffer_drain(tmp, -1));
 	tmp = bufferevent_get_input(evcon->bufev);
-	evbuffer_drain(tmp, evbuffer_get_length(tmp));
+	EVUTIL_ASSERT(!evbuffer_drain(tmp, -1));
 
 	evcon->state = EVCON_DISCONNECTED;
 }

commit 7c8999956fedc77decbd8f78192ed486c3fb1521

http: do not do function calls under EVUTIL_ASSERT() to fix NDEBUG builds
Fixes: 2185e639210f072f37e9d19aff7dba382db84529 ("http: assert's that
evbuffer_drain() success on connection reset")
Fixes: http/data_length_constraints
  FAIL ../test/regress_http.c:3775: assert(evhttp_request_get_response_code(req) == HTTP_ENTITYTOOLARGE): 501 vs 413

http.c
@@ -1314,6 +1314,7 @@ void
 evhttp_connection_reset_(struct evhttp_connection *evcon)
 {
 	struct evbuffer *tmp;
+	int err;
 
 	/* XXXX This is not actually an optimal fix.  Instead we ought to have
 	   an API for "stop connecting", or use bufferevent_setfd to turn off
@@ -1341,9 +1342,11 @@ evhttp_connection_reset_(struct evhttp_connection *evcon)
 
 	/* we need to clean up any buffered data */
 	tmp = bufferevent_get_output(evcon->bufev);
-	EVUTIL_ASSERT(!evbuffer_drain(tmp, -1));
+	err = evbuffer_drain(tmp, -1);
+	EVUTIL_ASSERT(!err && "drain output");
 	tmp = bufferevent_get_input(evcon->bufev);
-	EVUTIL_ASSERT(!evbuffer_drain(tmp, -1));
+	err = evbuffer_drain(tmp, -1);
+	EVUTIL_ASSERT(!err && "drain input");
 
 	evcon->state = EVCON_DISCONNECTED;
 }