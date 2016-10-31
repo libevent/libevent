libevent/libevent

commit 69cce257eb6183dda114d8f5b8ca0325cfabe2e1

autogen.sh: remove all autoconf/automake caches, if any
Otherwise if you for example share root of libevent repository with some
vm (for example windows+cygwin) then if you try to build with configure
on host, and then on guest it will fail, so let's just ignore this since
autogen.sh is just for this -- cleaning all stuff.

autogen.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+make maintainer-clean >/dev/null 2>/dev/null
+
 if [ -x "`which autoreconf 2>/dev/null`" ] ; then
    exec autoreconf -ivf
 fi

commit 9376ac4dff993a1852f5d4c1d3530deb758f1d02

autogen.sh: learn about gmake
Fixes: 69cce257eb6183dda114d8f5b8ca0325cfabe2e1
("69cce257eb6183dda114d8f5b8ca0325cfabe2e1")

autogen.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-make maintainer-clean >/dev/null 2>/dev/null
+MAKE=make
+if command -v gmake >/dev/null 2>/dev/null; then
+  MAKE=gmake
+fi
+$MAKE maintainer-clean >/dev/null 2>/dev/null
 
 if [ -x "`which autoreconf 2>/dev/null`" ] ; then
    exec autoreconf -ivf