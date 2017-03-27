303d6d77c7d66a69462d500857480f7e265eac07
Azat Khuzhin <a3at.mail@gmail.com>
2017-03-27 15:50:23 +0300

    Fix arc4random_addrandom() detecting and fallback (regression)
    But this is kind of hot-fix, we definitelly need more sane arc4random
    compat layer.
    
    Fixes: #488
    Introduced-in: 6541168 ("Detect arch4random_addrandom() existence")
    (cherry picked from commit 266f43af7798befa3d27bfabaa9ae699259c3924)

--- include/event2/util.h
+++ include/event2/util.h
@@ -842,7 +842,7 @@ int evutil_secure_rng_init(void);
 EVENT2_EXPORT_SYMBOL
 int evutil_secure_rng_set_urandom_device_file(char *fname);
 
-#ifdef EVENT__HAVE_ARC4RANDOM_ADDRANDOM
+#if !defined(EVENT__HAVE_ARC4RANDOM) || defined(EVENT__HAVE_ARC4RANDOM_ADDRANDOM)
 /** Seed the random number generator with extra random bytes.
 
     You should almost never need to call this function; it should be
