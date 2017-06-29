52bdb8b5b2bb8f9a6cb636f96b5929f33d633e6e
Wladimir J. van der Laan <laanwj@gmail.com>
2017-06-29 20:44:10 +0200

    http: Add EVHTTP_CON_OUTGOING when passing existing connection
    Suggested by Greg Hazel so the connection can read more than one
    request.
    
    Signed-off-by: Kirill Rodriguez <theoden8@tutamail.com>

--- http.c
+++ http.c
@@ -2583,6 +2583,7 @@ evhttp_connection_new_(struct event_base *base, struct bufferevent* bev)
 		 * can start sending requests on.
 		 */
 		evcon->state = EVCON_IDLE;
+		evcon->flags |= EVHTTP_CON_OUTGOING;
 	}
 
 	return (evcon);
