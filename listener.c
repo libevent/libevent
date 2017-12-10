42e851bb8c38e799bbd31fcf0a4c6b11fdb10df6
Azat Khuzhin <a3at.mail@gmail.com>
2017-12-10 23:57:19 +0300

    Merge branch 'evconnlistener-do-not-close-client-fd'
    Fixes: #577
    
    * evconnlistener-do-not-close-client-fd:
      listener: cover closing of fd in case evconnlistener_free() called from acceptcb
      Revert "Fix potential fd leak in listener_read_cb()"
    
    (cherry picked from commit bc65ffc14c62c10feffefe6c3d9975ce1d1a8cd8)

--- listener.c
+++ listener.c
@@ -421,8 +421,6 @@ listener_read_cb(evutil_socket_t fd, short what, void *p)
 		if (lev->refcnt == 1) {
 			int freed = listener_decref_and_unlock(lev);
 			EVUTIL_ASSERT(freed);
-
-			evutil_closesocket(new_fd);
 			return;
 		}
 		--lev->refcnt;
