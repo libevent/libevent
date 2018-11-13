libevent/libevent

commit af9b2a7ae0be11c79a909d212b1833a9379e4ba0

Initialize async bufferevent timeout CBs unconditionally
This is a fix for bug #93 on nmathewson/Libevent.

bufferevent_async.c
@@ -564,9 +564,9 @@ bufferevent_async_new_(struct event_base *base,
 	event_overlapped_init_(&bev_a->read_overlapped, read_complete);
 	event_overlapped_init_(&bev_a->write_overlapped, write_complete);
 
+	bufferevent_init_generic_timeout_cbs_(bev);
+
 	bev_a->ok = fd >= 0;
-	if (bev_a->ok)
-		bufferevent_init_generic_timeout_cbs_(bev);
 
 	return bev;
 err:

commit 2bf673a467ad412771419ea131cf2b3a9da740eb

bev_async: do not initialize timeouts multiple times
You cannot event_assign() event multiple times, this is UB, and most
likely will fail.

Fixes: af9b2a7ae0be11c79a909d212b1833a9379e4ba0 ("Initialize async
bufferevent timeout CBs unconditionally")

bufferevent_async.c
@@ -589,7 +589,6 @@ bufferevent_async_set_connected_(struct bufferevent *bev)
 {
 	struct bufferevent_async *bev_async = upcast(bev);
 	bev_async->ok = 1;
-	bufferevent_init_generic_timeout_cbs_(bev);
 	/* Now's a good time to consider reading/writing */
 	be_async_enable(bev, bev->enabled);
 }