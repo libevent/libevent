148d12ad31b03a813f4ffd9df14a85392aa74130
yuangongji <82787816@qq.com>
2019-09-19 15:24:51 +0800

    evutil_time: detect and use _gmtime64_s()/_gmtime64()

--- event-config.h.cmake
+++ event-config.h.cmake
@@ -292,6 +292,12 @@
 /* Define to 1 if you have the `strtoll' function. */
 #cmakedefine EVENT__HAVE_STRTOLL 1
 
+/* Define to 1 if you have the `_gmtime64_s' function. */
+#cmakedefine EVENT__HAVE__GMTIME64_S 1
+
+/* Define to 1 if you have the `_gmtime64' function. */
+#cmakedefine EVENT__HAVE__GMTIME64 1
+
 /* Define to 1 if the system has the type `struct addrinfo'. */
 #cmakedefine EVENT__HAVE_STRUCT_ADDRINFO 1
 
