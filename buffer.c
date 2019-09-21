libevent/libevent

commit 6a81b1f5a423aa5ec237c085c94bdc0a6cfd9e5d

Avoid double-free on error in evbuffer_add_file. Found by coverity.

buffer.c
@@ -3184,7 +3184,8 @@ evbuffer_add_file(struct evbuffer *buf, int fd, ev_off_t offset, ev_off_t length
 	if (!seg)
 		return -1;
 	r = evbuffer_add_file_segment(buf, seg, 0, length);
-	evbuffer_file_segment_free(seg);
+	if (r == 0)
+		evbuffer_file_segment_free(seg);
 	return r;
 }
 

commit 4727150a54e21725f0ef4f43ca5028cc6f353664

evbuffer_add_file: fix freeing of segment in the error path
if evbuffer_add_file_segment() fails it returns -1, so we should call
evbuffer_file_segment_free() only on error, and this -1 not 0.

Fixes: 6a81b1f5 ("Avoid double-free on error in evbuffer_add_file. Found by coverity.")
Backport-to: 2.1

buffer.c
@@ -3316,7 +3316,7 @@ evbuffer_add_file(struct evbuffer *buf, int fd, ev_off_t offset, ev_off_t length
 	if (!seg)
 		return -1;
 	r = evbuffer_add_file_segment(buf, seg, 0, length);
-	if (r == 0)
+	if (r == -1)
 		evbuffer_file_segment_free(seg);
 	return r;
 }