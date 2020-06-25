libevent/libevent

commit cfcc093606e747a5d250787012bac0b149c60d6d

Call `super` to call methods from the parent class
Not only does it make it clearer to the reader what the intent is behind
the code, it fixes MRO evaluation of classes if the parent-child
relationship is nonlinear.

Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

event_rpcgen.py
@@ -580,7 +580,7 @@ class Entry:
 class EntryBytes(Entry):
     def __init__(self, type, name, tag, length):
         # Init base class
-        Entry.__init__(self, type, name, tag)
+        super(EntryBytes, self).__init__(type, name, tag)
 
         self._length = length
         self._ctype = "ev_uint8_t"
@@ -687,13 +687,13 @@ class EntryBytes(Entry):
                 "around line %d" % (self._name, self.LineCount())
             )
 
-        Entry.Verify(self)
+        super(EntryBytes, self).Verify()
 
 
 class EntryInt(Entry):
     def __init__(self, type, name, tag, bits=32):
         # Init base class
-        Entry.__init__(self, type, name, tag)
+        super(EntryInt, self).__init__(type, name, tag)
 
         self._can_be_array = True
         if bits == 32:
@@ -752,7 +752,7 @@ class EntryInt(Entry):
 class EntryString(Entry):
     def __init__(self, type, name, tag):
         # Init base class
-        Entry.__init__(self, type, name, tag)
+        super(EntryString, self).__init__(type, name, tag)
 
         self._can_be_array = True
         self._ctype = "char *"
@@ -870,7 +870,7 @@ class EntryString(Entry):
 class EntryStruct(Entry):
     def __init__(self, type, name, tag, refname):
         # Init base class
-        Entry.__init__(self, type, name, tag)
+        super(EntryStruct, self).__init__(type, name, tag)
 
         self._optpointer = False
         self._can_be_array = True
@@ -1049,7 +1049,7 @@ class EntryStruct(Entry):
 class EntryVarBytes(Entry):
     def __init__(self, type, name, tag):
         # Init base class
-        Entry.__init__(self, type, name, tag)
+        super(EntryVarBytes, self).__init__(type, name, tag)
 
         self._ctype = "ev_uint8_t *"
 
@@ -1180,7 +1180,7 @@ class EntryVarBytes(Entry):
 class EntryArray(Entry):
     def __init__(self, entry):
         # Init base class
-        Entry.__init__(self, entry._type, entry._name, entry._tag)
+        super(EntryArray, self).__init__(entry._type, entry._name, entry._tag)
 
         self._entry = entry
         self._refname = entry._refname

commit 46c9ead0ebbbb23c8f719c11c473dfec37c84fda

Make all classes Entry, Struct, etc) new-style classes
This allows the object methods to be properly inherited and called via
`super(..)`, addressing breakage with python 2.x, introduced in
cfcc093606e747a5d250787012bac0b149c60d6d.

Fixes #1042.

Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

event_rpcgen.py
@@ -62,7 +62,7 @@ class RpcGenError(Exception):
 
 
 # Holds everything that makes a struct
-class Struct:
+class Struct(object):
     def __init__(self, name):
         self._name = name
         self._entries = []
@@ -422,7 +422,7 @@ evtag_marshal_%(name)s(struct evbuffer *evbuf, ev_uint32_t tag,
         )
 
 
-class Entry:
+class Entry(object):
     def __init__(self, ent_type, name, tag):
         self._type = ent_type
         self._name = name
@@ -1697,7 +1697,7 @@ def Parse(factory, filep):
     return entities
 
 
-class CCodeGenerator:
+class CCodeGenerator(object):
     def __init__(self):
         pass
 
@@ -1826,7 +1826,7 @@ class CCodeGenerator:
         return EntryArray(entry)
 
 
-class CommandLine:
+class CommandLine(object):
     def __init__(self, argv=None):
         """Initialize a command-line to launch event_rpcgen, as if
            from a command-line with CommandLine(sys.argv).  If you're