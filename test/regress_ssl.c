187f6b913b38a0cd5a0d9775f794fdf02c105953
Peter Edwards <peadar@arista.com>
2022-06-27 22:41:47 +0100

    OpenSSL 3 fixes: Disable TLS 1.3 when testing renegotiation support
    TLS 1.3 does not support renegotation - it has been removed from the
    protocol. Disable TLS 1.3 when testing this, so we negotiate an older
    protocol version

--- test/regress_ssl.c
+++ test/regress_ssl.c
@@ -93,6 +93,7 @@ static const char KEY[] =
     "-----END RSA PRIVATE KEY-----\n";
 
 static int disable_tls_11_and_12 = 0;
+static int disable_tls_13 = 0;
 static int test_is_done;
 static int n_connected;
 static int got_close;
@@ -323,6 +324,12 @@ regress_bufferevent_openssl(void *arg)
 	type = (enum regress_openssl_type)data->setup_data;
 
 	if (type & REGRESS_OPENSSL_RENEGOTIATE) {
+		/*
+		 * Disable TLS 1.3, so we negotiate something older to test
+		 * renegotiation - renegotiation is not supported by the
+		 * protocol any more.
+		 */
+		disable_tls_13 = 1;
 		if (OPENSSL_VERSION_NUMBER >= 0x10001000 &&
 		    OPENSSL_VERSION_NUMBER <  0x1000104f) {
 			/* 1.0.1 up to 1.0.1c has a bug where TLS1.1 and 1.2
