53d3c65601df2480a0f05d18785c77a1a55840ff
Dmitry Ilyin <dima@doty.ru>
2022-08-10 07:53:12 +0300

    Fix autoconf endianess detection issue

--- sha1.c
+++ sha1.c
@@ -29,11 +29,11 @@ A million repetitions of "a"
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
-#ifdef LITTLE_ENDIAN
+#if defined(LITTLE_ENDIAN)
 #define blk0(i)                                                                \
     (block->l[i] = (rol(block->l[i], 24) & 0xFF00FF00) |                       \
                    (rol(block->l[i], 8) & 0x00FF00FF))
-#elif BYTE_ORDER == BIG_ENDIAN
+#elif defined(BIG_ENDIAN)
 #define blk0(i) block->l[i]
 #else
 #error "Endianness not defined!"
