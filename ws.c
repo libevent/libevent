81fc23f9bc577ad83de8dd7c3e3e42514df13511
Dmitry Ilyin <dima@doty.ru>
2022-08-31 11:45:38 +0300

    Remove redundant evws->cb check

--- ws.c
+++ ws.c
@@ -281,14 +281,12 @@ ws_evhttp_read_cb(struct bufferevent *bufev, void *arg)
 		type = get_ws_frame(data, in_len, &payload, &msg_len);
 
 		if (type == TEXT_FRAME) {
-			if (evws->cb) {
-				char *text = mm_malloc(msg_len + 1);
+			char *text = mm_malloc(msg_len + 1);
 
-				memcpy(text, payload, msg_len);
-				text[msg_len] = 0;
-				evws->cb(evws, text, msg_len, evws->cb_arg);
-				free(text);
-			}
+			memcpy(text, payload, msg_len);
+			text[msg_len] = 0;
+			evws->cb(evws, text, msg_len, evws->cb_arg);
+			free(text);
 		} else if (type == INCOMPLETE_TEXT_FRAME || type == INCOMPLETE_FRAME) {
 			/* incomplete frame received, wait for next chunk */
 			return;
