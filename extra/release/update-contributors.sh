dfbb004a4ae0bbf225173854ca71ec0ea761cd9c
Azat Khuzhin <azat@libevent.org>
2022-11-20 23:11:00 +0100

    Fix script for updating contributors

--- extra/release/update-contributors.sh
+++ extra/release/update-contributors.sh
@@ -12,13 +12,13 @@ function main()
 
     git log "$(git describe --abbrev=0)..HEAD" --pretty='format:%cN%n%aN' > "$new_contributors"
     awk '/^ \* / { split($0, cols, " \\* "); print(cols[2]); }' "$(path_in_repo CONTRIBUTORS.md)" | {
-        grep -F -x -v -f "$new_contributors"
+        grep -F -x -v -f- "$new_contributors"
     } | {
         local grep_patterns=(
-            GitHub
+            -e GitHub
         )
         grep -F -x -v "${grep_patterns[@]}" 
-    } > "$new_contributors.filtered"
+    } | sort -u > "$new_contributors.filtered"
     awk '{printf(" * %s\n", $0)}' "$new_contributors.filtered" >> "$(path_in_repo CONTRIBUTORS.md)"
 }
 main "$@"
