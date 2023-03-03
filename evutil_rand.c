915f0cdf659fc9ad1c7cd0196803c4d4f856f457
Kurt Roeckx <kurt@roeckx.be>
2023-03-03 19:22:46 +0100

    Always have evutil_secure_rng_add_bytes available
    When libevent doesn't provide random the arc4 function, but they come from libc, there is no need to call this function, so make it do nothing.
    
    Fixes: #1393

--- evutil_rand.c
+++ evutil_rand.c
@@ -190,14 +190,14 @@ evutil_secure_rng_get_bytes(void *buf, size_t n)
 	ev_arc4random_buf(buf, n);
 }
 
-#if !defined(EVENT__HAVE_ARC4RANDOM) || defined(EVENT__HAVE_ARC4RANDOM_ADDRANDOM)
 void
 evutil_secure_rng_add_bytes(const char *buf, size_t n)
 {
+#if !defined(EVENT__HAVE_ARC4RANDOM) || defined(EVENT__HAVE_ARC4RANDOM_ADDRANDOM)
 	arc4random_addrandom((unsigned char*)buf,
 	    n>(size_t)INT_MAX ? INT_MAX : (int)n);
-}
 #endif
+}
 
 void
 evutil_free_secure_rng_globals_(void)
