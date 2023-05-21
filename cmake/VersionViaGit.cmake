abc114e3c8b57f071ea07e06d3a3e6f35ad5b6be
Azat Khuzhin <azat@libevent.org>
2023-05-21 12:49:34 +0200

    Bump default version when git tag is incorrect
    Fixes: #1457

--- cmake/VersionViaGit.cmake
+++ cmake/VersionViaGit.cmake
@@ -23,7 +23,7 @@ macro(event_fuzzy_version_from_git)
 	# set our defaults.
 	set(EVENT_GIT___VERSION_MAJOR 2)
 	set(EVENT_GIT___VERSION_MINOR 2)
-	set(EVENT_GIT___VERSION_PATCH 0)
+	set(EVENT_GIT___VERSION_PATCH 1)
 	set(EVENT_GIT___VERSION_STAGE "alpha-dev")
 
 	find_package(Git)
