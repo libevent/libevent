9d4853b363e3bd94c957163253de11b2a08b7c2f
Andy Pan <panjf2000@gmail.com>
2024-01-13 20:51:49 +0800

    Optimize preprocessor directives on Solaris for evutil_set_tcp_keepalive (#1536)
    Changes:
    - Optimize preprocessor directives on Solaris for evutil_set_tcp_keepalive
    - Reduce the amount of code being compiled and trim trailing whitespace in passing.
    - Move comments for Solaris under its macro
    - Refactor the control flow

--- include/event2/util.h
+++ include/event2/util.h
@@ -473,7 +473,7 @@ int evutil_closesocket(evutil_socket_t sock);
 
 /** Do platform-specific operations, if possible, to make a tcp listener
  *  socket defer accept()s until there is data to read.
- *  
+ *
  *  Not all platforms support this.  You don't want to do this for every
  *  listener socket: only the ones that implement a protocol where the
  *  client transmits before the server needs to respond.
@@ -481,19 +481,19 @@ int evutil_closesocket(evutil_socket_t sock);
  *  @param sock The listening socket to to make deferred
  *  @return 0 on success (whether the operation is supported or not),
  *       -1 on failure
-*/ 
+*/
 EVENT2_EXPORT_SYMBOL
 int evutil_make_tcp_listen_socket_deferred(evutil_socket_t sock);
 
 /** Do platform-specific operations to set/unset TCP keep-alive options
  * TCP_KEEPIDLE, TCP_KEEPINTVL and TCP_KEEPCNT on a socket.
- *  
- *  @param sock The socket to be set TCP keep-alive 
- *  @param on nonzero value to enable TCP keep-alive, 0 to disable
- *  @param timeout The timeout in seconds with no activity until 
+ *
+ *  @param sock The socket to be set TCP keep-alive
+ *  @param on Nonzero value to enable TCP keep-alive, 0 to disable
+ *  @param timeout The timeout in seconds with no activity until
  * 	   the first keepalive probe is sent
  *  @return 0 on success, -1 on failure
-*/ 
+*/
 EVENT2_EXPORT_SYMBOL
 int evutil_set_tcp_keepalive(evutil_socket_t sock, int on, int timeout);
 
