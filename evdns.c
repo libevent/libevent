e95bdbbc87567d9decc68dd90ecdbf19a525cdd7
Kirill Rodriguez <theoden8@tutamail.com>
2024-09-25 12:18:28 +0100

    fix unused variable

--- evdns.c
+++ evdns.c
@@ -5361,7 +5361,6 @@ static void
 evdns_ttl_expired(int result, short what, void *arg)
 {
 	struct evdns_cache *cache = arg;
-	struct evdns_base *base = cache->base;
 	log(EVDNS_LOG_DEBUG, "Expiring cache for %s", cache->name);
 	EVDNS_LOCK(base);
 	evdns_cache_free(cache);
