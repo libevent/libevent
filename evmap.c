libevent/libevent

commit 91e3ead85eb81c3f8aabb8a193e3cc952755d87a

Improve the hashsocket function.
svn:r989

evmap.c
@@ -91,7 +91,12 @@ struct event_map_entry {
 static inline unsigned
 hashsocket(struct event_map_entry *e)
 {
-	return (unsigned) e->fd;
+	/* On win32, in practice, the low 2-3 bits of a SOCKET seem not to
+	 * matter.  Our hashtable implementation really likes low-order bits,
+	 * though, so let's do the rotate-and-add trick. */
+	unsigned h = (unsigned) e->fd;
+	h += (h >> 2) | (h << 30);
+	return h;
 }
 
 static inline int

commit ba0442bacc4b5622b203226f7c96f3dff7a90977

Improve hashsocket() (for win32) to avoid collisions
Fixes: 91e3ead
Refs: https://gist.github.com/azat/8a0a94e4de2d95af8f729b709589ce12

evmap.c
@@ -95,7 +95,7 @@ hashsocket(struct event_map_entry *e)
 	 * matter.  Our hashtable implementation really likes low-order bits,
 	 * though, so let's do the rotate-and-add trick. */
 	unsigned h = (unsigned) e->fd;
-	h += (h >> 2) | (h << 30);
+	h = (h >> 2) | (h << 30);
 	return h;
 }
 