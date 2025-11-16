92e645466ddb31fa0b8113973f44a0271240995b
Azat Khuzhin <azat@libevent.org>
2025-11-16 22:58:17 +0100

    Fix -Wunterminated-string-initialization in evutil_sockaddr_is_loopback_()
    Error:
    
        /src/le/libevent/evutil.c:2783:6: warning: initializer-string for character array is too long, array size is 16 but initializer has size 17 (including the null terminating character); did you mean to use the 'nonstring' attribute? [-Wunterminated-string-initialization]
         2783 |             "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\1";
              |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- evutil.c
+++ evutil.c
@@ -2779,8 +2779,10 @@ evutil_memclear_(void *mem, size_t len)
 int
 evutil_sockaddr_is_loopback_(const struct sockaddr *addr)
 {
-	static const char LOOPBACK_S6[16] =
-	    "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\1";
+	static const char LOOPBACK_S6[16] = {
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 1
+	};
 	if (addr->sa_family == AF_INET) {
 		struct sockaddr_in *sin = (struct sockaddr_in *)addr;
 		return (ntohl(sin->sin_addr.s_addr) & 0xff000000) == 0x7f000000;
