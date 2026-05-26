1b7cc9b58d6a5fdd5f33a43168245c2e1f35291f
Azat Khuzhin <azat@libevent.org>
2026-05-26 22:58:31 +0200

    Fix https-client build under mingw (strndup is presented there) (#1863)

--- sample/https-client.c
+++ sample/https-client.c
@@ -252,7 +252,7 @@ add_cert_for_store(X509_STORE *store, const char *name)
 }
 #endif
 
-#if defined(_WIN32)
+#if defined(_WIN32) && !defined(__MINGW32__)
 static char* strndup(const char* src, size_t chars)
 {
 	char* buffer = (char*) malloc(chars + 1);
